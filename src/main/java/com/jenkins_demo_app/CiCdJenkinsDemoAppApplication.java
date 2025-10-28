192246fce1e3c906d44f9f8799cf04bc5408e5f6
urvish16 <urvishsoni96@gmail.com>
2025-10-28 08:31:02 +0530

    ci-cd pipeline demo app

---
+++ src/main/java/com/jenkins_demo_app/CiCdJenkinsDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins_demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdJenkinsDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdJenkinsDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins_demo_app/CiCdJenkinsDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins_demo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdJenkinsDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
